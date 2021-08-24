4d4e0750ccc7994484a7602eaa6639fb71c3966e
Jordan Kim Jubilo <67796359+BlissCoders@users.noreply.github.com>
2021-08-25 04:20:54 +0800

    Add files via upload

---
+++ My First Web UI Project/Test Suites/New Test Suite.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>a3b54c32-89ad-4237-8b5e-76a4024c211f</testSuiteGuid>
+   <testCaseLink>
+      <guid>4c2445e0-70e0-4657-afa7-8b2e8b84b309</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2b02d331-6b2d-4af0-bc47-a5cb2ea39163</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test2</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
